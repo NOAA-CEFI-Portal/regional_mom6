2af9b771aab59dc0e5c253a08334302781a91d77
C.-W. HSU <11950284+chiaweh2@users.noreply.github.com>
2024-04-02 16:29:25 -0600

    testing regridded tercile calculation

---
+++ mom6_regrid_tercile.py
@@ -0,0 +1,71 @@
+"""
+This script is designed to calculate the tercile value based on the
+REGRIDDED forecast/hindcast data. 
+
+"""
+# %%
+import sys
+import warnings
+import numpy as np
+import xarray as xr
+from dask.distributed import Client
+from mom6_regrid import mom6_hindcast,mom6_encoding_attr
+warnings.simplefilter("ignore")
+
+# %%
+if __name__=="__main__":
+
+    client = Client(processes=False,memory_limit='150GB',silence_logs=50)
+    print(client)
+    print(client.cluster.dashboard_link)
+
+    # check argument exist
+    if len(sys.argv) < 2:
+        print("Usage: python mom6_regrid_tercile.py VARNAME")
+        sys.exit(1)
+    else:
+        varname = sys.argv[1]
+
+    # data locations    
+    mom6_dir = "/Datasets.private/regional_mom6/hindcast/regrid/"
+    mom6_tercile_dir = "/Datasets.private/regional_mom6/tercile_calculation/regrid/"
+    file_list = mom6_hindcast(mom6_dir)
+    var_file_list = []
+    for file in file_list :
+        if varname in file :
+            var_file_list.append(file)
+
+    # open regrid file
+    for file in var_file_list :
+        ds = xr.open_dataset(file)
+
+        # calculate the tercile value
+        da_tercile = (
+            ds[varname]
+            .stack(allens=('init','member'))
+            .quantile(
+                [1./3.,2./3.],
+                dim='allens',
+                keep_attrs=True
+            )
+        )
+     
+        ds_tercile = xr.Dataset()
+        ds_tercile.attrs = ds.attrs
+        ds_tercile['f_lowmid'] = da_tercile.isel(quantile=0)
+        ds_tercile['f_midhigh'] = da_tercile.isel(quantile=1)
+        ds_tercile = ds_tercile.drop_vars('quantile')
+
+        # output the netcdf file
+        print(f'output {mom6_tercile_dir}{file[len(mom6_dir):]}')
+        mom6_encoding_attr(
+                ds,
+                ds_tercile,
+                var_names=list(ds_tercile.keys()),
+                dataset_name='regional mom6 regrid tercile'
+            )
+        try:
+            ds_tercile.to_netcdf(f'{mom6_tercile_dir}{file[len(mom6_dir):]}',mode='w')
+        except PermissionError:
+            print(f'{mom6_tercile_dir}{file[len(mom6_dir):]} is used by other scripts' )
+
