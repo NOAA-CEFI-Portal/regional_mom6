48ac32d693671af6f396416de21844bd54018fe5
C.-W. HSU <11950284+chiaweh2@users.noreply.github.com>
2024-08-30 16:28:14 -0600

    add mhw and statistic testing

--- tests/conftest.py
+++ tests/conftest.py
@@ -9,6 +9,8 @@ To do pytest on the local machine
 
 """
 import pytest
+import xarray as xr
+from mom6.mom6_module import mom6_io
 
 def pytest_addoption(parser):
     parser.addoption("--location", action="store", default="opendap")
@@ -16,3 +18,27 @@ def pytest_addoption(parser):
 @pytest.fixture
 def location(request):
     return request.config.getoption("--location")
+
+# Define the fixture that loads data
+# 'session' scope allowed data to be shared across test session
+@pytest.fixture
+def ds_forecast(location)->xr.Dataset:
+    """load data for testing
+
+    Returns
+    -------
+    xr.Dataset
+        The loaded data in dataset
+    """
+    # loading the forecast
+    forecast_io_class = mom6_io.MOM6Forecast(
+        var='tos',
+        data_relative_dir='forecast/',
+        static_relative_dir='static/',
+        grid='raw',
+        source=location
+    )
+
+    ds = forecast_io_class.get_all()
+
+    return ds
