ae3f22ddf6beae631bf2b7fde41169c6a8d4be14
C.-W. HSU <11950284+chiaweh2@users.noreply.github.com>
2025-02-18 08:41:20 -0700

    finish tercile probability method

--- mom6/mom6_module/mom6_forecast_tercile.py
+++ mom6/mom6_module/mom6_forecast_tercile.py
@@ -124,156 +124,133 @@ class Tercile:
 
         return ds_terciles
 
-
-    # def calculate_tercile_prob(
-    #     self,
-    #     iyear : int = 1993,
-    #     imonth: int = 3,
-    #     lead_bins : List[int] = None,
-    #     lead_bin : Union[int, float] = None,
-    #     lon : Union[int, float] = None,
-    #     lat : Union[int, float] = None
-    # ) -> xr.Dataset:
-    #     """
-    #     use single initialization's normal distribution
-    #     and pre-defined tercile value based on the long-term 
-    #     statistic tercile value to find the probability of
-    #     upper ,normal , and lower tercile
+    @staticmethod
+    def generate_tercile_probability(
+        ds_single_initialization : xr.Dataset,
+        varname : str,
+        ds_tercile : xr.Dataset,
+        lead_bins : List[int] = None,
+    ) -> xr.Dataset:
+        """_use single initialization's normal distribution
+        and pre-determined tercile value based on the long-term 
+        reforecast to find the probability of upper, middle,
+        and lower tercile
         
-    #     It also find the largest probability in upper (positive),
-    #     normal (0), lower (negative)
+        It also find the largest probability in upper (positive),
+        middle (0), and lower (negative)
 
-    #     Parameters
-    #     ----------
-    #     lead_bins : List[int]
-    #         The `lead_bin` used to binned the leading month result
-    #         ex: one can set `lead_bins = [0, 3, 6, 9, 12]` for four seasonal
-    #         mean. Default is no binning, lead_bins = None.
-        
-    #     Returns
-    #     -------
-    #     xr.Dataset
-    #         two variables are in the dataset. (1) tercile_prob 
-    #         (2) tercile_prob_max. 
+        Parameters
+        ----------
+        ds_tercile : xr.Dataset
+            The tercile data which has the f_lowmid, f_midhigh calculated 
+            from `generate_tercile` method.
+        lead_bins : List[int], optional
+            The `lead_bins` used to binned the leading month result
+            ex: one can set `lead_bins = [0, 3, 6, 9, 12]` for four seasonal
+            mean. Default is no binning, lead_bins = None.
 
-    #         1 is a 4D matrix with the dimension 
-    #         of lon x lat x lead x 3. This are the probability of
-    #         upper(lon x lat x lead), normal(lon x lat x lead),
-    #         and lower tercile(lon x lat x lead)
+        Returns
+        -------
+        xr.Dataset
+            two variables are in the dataset. 
+            (1) tercile_prob 
+            (2) tercile_prob_max. 
 
-    #         2 is the 3D matrix of largest probability in upper (positive),
-    #         normal (0), lower (negative) with dimension of (lon x lat x lead)
-    #     """
+            1 is a 4D matrix with the dimension 
+            of lon x lat x lead x 3. This are the probability of
+            upper(lon x lat x lead), middle(lon x lat x lead),
+            and lower tercile(lon x lat x lead)
 
-    #     # loaded the mom6 raw field
-    #     GetFcast = mom6_io.MOM6Forecast(
-    #         var = self.var,
-    #         data_relative_dir = self.data_relative_dir,
-    #         static_relative_dir = self.static_relative_dir,
-    #         tercile_relative_dir = self.tercile_relative_dir,
-    #         grid = self.grid,
-    #         source = self.source,
-    #     )
+            2 is the 3D matrix of largest probability in upper (positive),
+            middle (0), lower (negative) with dimension of (lon x lat x lead)
 
-    #     ds_data = GetFcast.get_single(
-    #         iyear = iyear,
-    #         imonth = imonth
-    #     )
+        Raises
+        ------
+        LookupError
+            when the input ds_single_initialization has more than one variable
+        """
 
-    #     # load variable to memory
-    #     da_data = ds_data[self.var]
+        # getting the single initialization
+        ds_data = ds_single_initialization
 
-    #     if lead_bins is None:
-    #         # average the forecast over the lead bins
-    #         da_binned = da_data.rename({'lead': 'lead_bin'})
-    #     else:
-    #         # setup lead bins to average during forecast lead time
-    #         # (should match lead bins used for the historical data
-    #         # that created the *_terciles_*.nc
-    #         # [0, 3, 6, 9, 12] produces 3-month averages
-    #         lead_bin_label = np.arange(0,len(lead_bins)-1)
+        # load variable to memory
+        da_data = ds_data[varname]
 
-    #         # average the forecast over the lead bins
-    #         da_binned = (
-    #             da_data
-    #             .groupby_bins('lead', lead_bins, labels=lead_bin_label, right=True)
-    #             .mean('lead')
-    #             .rename({'lead_bins': 'lead_bin'})
-    #         )
+        # find initial month
+        imonth = da_data['init.month'].data
 
-    #     if lead_bin is not None:
-    #         da_binned = da_binned.sel(lead_bin=lead_bin,method='nearest')
-    #     if lon is not None:
-    #         da_binned = da_binned.sel(lon=lon,method='nearest')
-    #     if lat is not None:
-    #         da_binned = da_binned.sel(lat=lat,method='nearest')
+        if lead_bins is None:
+            # 1 lead 1 bin
+            da_binned = da_data.rename({'lead': 'lead_bin'})
+        else:
+            # setup lead bins to average during forecast lead time
+            lead_bin_label = np.arange(0,len(lead_bins)-1)
 
-    #     # find a normal distribution for each grid cell and lead bin
-    #     # from the ensemble mean and standard deviation
-    #     #  this is based on 1 initialization
-    #     da_mean = da_binned.mean('member')
-    #     da_std = da_binned.std('member')
-    #     da_dist = normal(loc=da_mean, scale=da_std)
+            # average the forecast over the lead bins
+            da_binned = (
+                da_data
+                .groupby_bins('lead', lead_bins, labels=lead_bin_label, right=True)
+                .mean('lead')
+                .rename({'lead_bins': 'lead_bin'})
+            )
 
-    #     # load the predetermined reforecast/forecast tercile value
-    #     #  this is based on 30 years statistic 1993-2023
-    #     ds_tercile = GetFcast.get_tercile('grid')
-    #     ds_tercile = ds_tercile.sel(init=imonth,method='nearest')
+        # find a normal distribution for each grid cell and lead bin
+        # from the ensemble mean and standard deviation
+        #  this is based on single initialization
+        da_mean = da_binned.mean('member')
+        da_std = da_binned.std('member')
+        da_dist = normal(loc=da_mean, scale=da_std)
 
-    #     if lead_bins is None:
-    #         ds_tercile_binned = ds_tercile.rename({'lead': 'lead_bin'})
-    #     else:
-    #         # average the forecast over the lead bins
-    #         ds_tercile_binned = (
-    #             ds_tercile
-    #             .groupby_bins('lead', lead_bins, labels=lead_bin_label, right=True)
-    #             .mean('lead')
-    #             .rename({'lead_bins': 'lead_bin'})
-    #         )
+        # load the predetermined reforecast/forecast tercile value
+        ds_tercile = ds_tercile.sel(month=imonth,method='nearest')
 
-    #     if lead_bin is not None:
-    #         ds_tercile_binned = ds_tercile_binned.sel(lead_bin=lead_bin,method='nearest')
-    #     if lon is not None:
-    #         ds_tercile_binned = ds_tercile_binned.sel(lon=lon,method='nearest')
-    #     if lat is not None:
-    #         ds_tercile_binned = ds_tercile_binned.sel(lat=lat,method='nearest')
+        if lead_bins is None:
+            ds_tercile_binned = ds_tercile.rename({'lead': 'lead_bin'})
+        else:
+            # average the forecast over the lead bins
+            ds_tercile_binned = (
+                ds_tercile
+                .groupby_bins('lead', lead_bins, labels=lead_bin_label, right=True)
+                .mean('lead')
+                .rename({'lead_bins': 'lead_bin'})
+            )
 
-    #     # use single initialization's normal distribution
-    #     # and pre-defined tercile value to find the
-    #     # probability based on the single initialization
-    #     # that correspond to the long-term statistic tercile value
+        # use single initialization's normal distribution
+        # and pre-defined tercile value to find the
+        # probability based on the single initialization
+        # that correspond to the long-term statistic tercile value
 
-    #     #---probability of lower tercile tail
-    #     da_low_tercile_prob = xr.DataArray(
-    #         da_dist.cdf(ds_tercile_binned['f_lowmid']),
-    #         dims=da_mean.dims,
-    #         coords=da_mean.coords
-    #     )
-    #     #---probability of upper tercile tail
-    #     da_up_tercile_prob = 1 - xr.DataArray(
-    #         da_dist.cdf(ds_tercile_binned['f_midhigh']),
-    #         dims=da_mean.dims,
-    #         coords=da_mean.coords
-    #     )
-    #     #---probability of between lower and upper tercile
-    #     da_mid_tercile_prob = 1 - da_up_tercile_prob - da_low_tercile_prob
+        #---probability of lower tercile tail
+        da_low_tercile_prob = xr.DataArray(
+            da_dist.cdf(ds_tercile_binned['f_lowmid']),
+            dims=da_mean.dims,
+            coords=da_mean.coords
+        )
+        #---probability of upper tercile tail
+        da_up_tercile_prob = 1 - xr.DataArray(
+            da_dist.cdf(ds_tercile_binned['f_midhigh']),
+            dims=da_mean.dims,
+            coords=da_mean.coords
+        )
+        #---probability of between lower and upper tercile
+        da_mid_tercile_prob = 1 - da_up_tercile_prob - da_low_tercile_prob
 
-    #     da_tercile_prob = xr.concat(
-    #         [da_low_tercile_prob,da_mid_tercile_prob,da_up_tercile_prob],
-    #         pd.Index([-1,0,1],name="tercile")
-    #     )
+        da_tercile_prob = xr.concat(
+            [da_low_tercile_prob,da_mid_tercile_prob,da_up_tercile_prob],
+            pd.Index([-1,0,1],name="tercile")
+        )
 
-    #     # lower tercile max => negative
-    #     # nomral tercile max => 0
-    #     # upper tercile max => positive
-    #     da_tercile_prob_max = (
-    #         da_tercile_prob.idxmax(dim='tercile',fill_value=np.nan)*
-    #         da_tercile_prob.max(dim='tercile')
-    #     )
+        # lower tercile max => negative
+        # middle tercile max => 0  (normal does not need the prob value)
+        # upper tercile max => positive
+        da_tercile_prob_max = (
+            da_tercile_prob.idxmax(dim='tercile',fill_value=np.nan)*
+            da_tercile_prob.max(dim='tercile')
+        )
 
-    #     # create dataset to store the tercile calculation
-    #     ds_tercile_prob=xr.Dataset()
-    #     ds_tercile_prob['tercile_prob'] = da_tercile_prob
-    #     ds_tercile_prob['tercile_prob_max'] = da_tercile_prob_max
+        # create dataset to store the tercile calculation
+        ds_tercile_prob=xr.Dataset()
+        ds_tercile_prob['tercile_prob'] = da_tercile_prob
+        ds_tercile_prob['tercile_prob_max'] = da_tercile_prob_max
 
-    #     return ds_tercile_prob
+        return ds_tercile_prob
