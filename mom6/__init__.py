e3729e670973120b6034095e4e2f2d977accba12
C.-W. HSU <11950284+chiaweh2@users.noreply.github.com>
2025-05-06 13:56:28 -0600

    remove the need of config file

--- mom6/__init__.py
+++ mom6/__init__.py
@@ -1,17 +1,1 @@
-import json
-import os
 
-# Get the directory of the root of the project
-dir_path = os.path.dirname(
-    os.path.dirname(
-        os.path.realpath(__file__)
-    )
-)
-
-# open config setting
-with open(os.path.join(dir_path, 'config.json'), encoding="utf-8") as f:
-    config = json.load(f)
-
-# assign universal constant used in the package
-DATA_PATH = config['data_path']
-DATA_BASE = config['data_base']
